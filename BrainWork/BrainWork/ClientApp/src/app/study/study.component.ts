09d96e2780126a54f7540b93ef32b0b08c17ab56
Kristel Kahr <krkahr@ttu.ee>
2019-03-20 00:19:12 +0200

    study flip card (html+css)

--- BrainWork/BrainWork/ClientApp/src/app/study/study.component.ts
+++ BrainWork/BrainWork/ClientApp/src/app/study/study.component.ts
@@ -6,11 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./study.component.css']
 })
 export class StudyComponent implements OnInit {
-
   constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-
-  }
 
 }
