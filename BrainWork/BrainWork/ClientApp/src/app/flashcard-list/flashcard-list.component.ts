cfe0ff108004a0cd30cdf1fe47deee855f8bca8b
Sandra Kukk <sankuk@ttu.ee>
2019-04-03 12:23:05 +0300

    Routimine Create lehelt your stude setti

--- BrainWork/BrainWork/ClientApp/src/app/flashcard-list/flashcard-list.component.ts
+++ BrainWork/BrainWork/ClientApp/src/app/flashcard-list/flashcard-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Flashcard } from "../models/Flashcard";
 import { Set } from "../models/Set";
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-flashcard-list',
   templateUrl: './flashcard-list.component.html',
@@ -10,10 +13,17 @@ export class FlashcardListComponent implements OnInit {
 
   set: Flashcard[];
   setsList: Set[];
+  baseUrl: string;
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
-  constructor() {
+  constructor(private router: Router, private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.set = [];
     this.setsList = [];
+    this.baseUrl = baseUrl;
   }
 
   ngOnInit() {
@@ -35,9 +45,27 @@ export class FlashcardListComponent implements OnInit {
     
     let flashcardSet = new Set(title, this.set);
     this.setsList.push(flashcardSet);
-    console.dir(this.setsList[0]);
+
+    this.http.post(this.baseUrl + 'api/YourStudySets',
+      {
+        "title": title
+      }).subscribe(
+        (val) => {
+          console.log("post call successful", val);
+        },
+        response => {
+          console.log("post call error", response);
+        },
+        () => {
+          console.log("post is completed");
+          this.router.navigateByUrl('/sets-list');
+
+        }
+      );
   }
-  
-  
+}
+
+interface YourStudySet {
+  title: string;
 }
 
