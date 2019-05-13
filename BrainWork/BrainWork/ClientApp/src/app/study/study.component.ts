d6b5e2fcead92a1355e7c22aa127297c084d68f7
Sandra Kukk <sankuk@ttu.ee>
2019-05-13 22:29:58 +0300

    Study lehe HTML

--- BrainWork/BrainWork/ClientApp/src/app/study/study.component.ts
+++ BrainWork/BrainWork/ClientApp/src/app/study/study.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { YourStudySet } from '../flashcard-list/flashcard-list.component';
 
 @Component({
   selector: 'app-study',
@@ -6,11 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./study.component.css']
 })
 export class StudyComponent implements OnInit {
+  setItem: YourStudySet;
+  baseUrl: string;
 
-
-  constructor() { }
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.baseUrl = baseUrl;
+    
   }
+
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.http.get<YourStudySet>(this.baseUrl + 'api/YourStudySets/' + id).subscribe(res => this.setItem = res);
+    
+  }
+
+
+
+
 
   myFlipFunction() {
     var myElement = document.getElementById("thecard");
--- BrainWork/BrainWork/Models/YourStudySet.cs
+++ BrainWork/BrainWork/Models/YourStudySet.cs
@@ -10,8 +10,22 @@ namespace BrainWork.Models
         public int Id { get; set; }
         public string Title { get; set; }
         //public Flashcard[] FlashcardSet = new Flashcard[] { new Flashcard { Term = "testiterm", Definition = "testidef" } };
-        
-        public IList<Flashcard>  FlashcardSet { get; set; }
+
+        //public IList<Flashcard>  FlashcardSet { get; set; }
+
+        public List<Flashcard> FlashcardSet = new List<Flashcard>
+        {
+            new Flashcard{Term="Vabadussõda", Definition="1918 a"},
+            new Flashcard{Term="Jüriöö ülestõus", Definition="1343 a"},
+            new Flashcard{Term="Esimene maailmasõda", Definition="1914 a"},
+            new Flashcard{Term="Teine maailmasõda", Definition="1939 a"},
+            new Flashcard{Term="Põhjasõda", Definition="1700 a"},
+            new Flashcard{Term="Prantsuse revolutsioon", Definition="1789 a"},
+            new Flashcard{Term="Antiikaeg", Definition="Ajalooperiood, mis kestis Lääne-Riima riigi langusest uusaja riikide tekkeni (V - XVI saj.)"},
+            new Flashcard{Term="Vasall", Definition="Feodaal, kes sai oma senjöörilt maad"},
+            new Flashcard{Term="Loonusrent", Definition="Koormis, mida talupeog tasus oma põllult kogutud saagiga"}
+
+        };
 
     }
 }
